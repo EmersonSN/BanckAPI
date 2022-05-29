222eb1416dd69ca22e38470f75419c2c46834f78
Emerson <emersonn956@gmail.com>
2022-05-29 10:26:04 -0300

    Starting repo and add dependencys

---
+++ banckaccount/src/main/kotlin/com/example/banckaccount/BanckaccountApplication.kt
@@ -0,0 +1,11 @@
+package com.example.banckaccount
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BanckaccountApplication
+
+fun main(args: Array<String>) {
+	runApplication<BanckaccountApplication>(*args)
+}
