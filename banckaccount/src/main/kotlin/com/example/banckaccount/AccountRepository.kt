9bcb6c495b65913dc907bedd7d39a06c56575c06
Emerson <emersonn956@gmail.com>
2022-06-07 21:47:25 -0300

    Create controller class and create endpoint "/accounts"

--- banckaccount/src/main/kotlin/com/example/banckaccount/AccountRepository.kt
+++ banckaccount/src/main/kotlin/com/example/banckaccount/AccountRepository.kt
@@ -4,7 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository
 import java.util.Optional
 
 //Interface que extende o mongo que vem do Spring
-interface AccountRespository : MongoRepository <Account, String> {
+interface AccountRepository : MongoRepository <Account, String> {
 
     fun finBtDocument(document: String): Optional<Account>
 }
