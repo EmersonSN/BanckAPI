881f46c57b5b7cec2867dd84f51853ec33b7d0a4
Emerson <emersonn956@gmail.com>
2022-06-07 21:51:26 -0300

    Create read endpoint "/accounts"

--- banckaccount/src/main/kotlin/com/example/banckaccount/AccountController.kt
+++ banckaccount/src/main/kotlin/com/example/banckaccount/AccountController.kt
@@ -1,6 +1,7 @@
 package com.example.banckaccount
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,4 +13,7 @@ class AccountController(val repository: AccountRepository) {
 
     @PostMapping
     fun create(@RequestBody account: Account) = ResponseEntity.ok(repository.save(account))
+
+    @GetMapping
+    fun read() = ResponseEntity.ok(repository.findAll())
 }
